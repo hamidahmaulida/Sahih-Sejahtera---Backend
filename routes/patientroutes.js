61eebefc8cf927877d04560260437dc5e4a1da83
hamidahmaulidaa <142779349+hamidahmaulidaa@users.noreply.github.com>
2023-10-09 19:00:08 +0700

    fix

--- routes/patientroutes.js
+++ routes/patientroutes.js
@@ -1,4 +1,3 @@
-// routes/patientRoutes.js
 import express from "express";
 import { createPatient, getPatient, getAllPatients, updatePatients, deletePatient } from "../controllers/PatientControllers.js";
 
