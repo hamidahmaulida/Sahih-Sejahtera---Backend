924b77d5540ebfcccad53aa37de188768f25956d
hamidahmaulidaa <142779349+hamidahmaulidaa@users.noreply.github.com>
2023-10-10 09:35:04 +0700

    adding database port in database connection

--- config/database.js
+++ config/database.js
@@ -6,8 +6,9 @@ const db = new Sequelize(
     process.env.DATABASE_USERNAME,
     process.env.DATABASE_PASSWORD,
     {
-        host: process.env.DATABASE_HOST,
+    host: process.env.DATABASE_HOST,
     dialect: "mysql",
+    port: process.env.DATABASE_PORT,
 });
 
 export default db;
